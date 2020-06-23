315cab9094cad2cbc51afe3d660a15c7ac7cec57
yunqian44 <yunqian8@live.com>
2020-06-23 10:03:31 +0800

    初始化

---
+++ Azure.Active.DirectoryB2C.WebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Azure.Active.DirectoryB2C.WebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
